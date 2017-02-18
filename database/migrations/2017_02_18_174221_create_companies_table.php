7dd7cd90d1ca006d9d598965d3071aed48983177
Juaum Cruz <juaum.cruz@gmail.com>
2017-02-18 14:56:42 -0300

    .

--- database/migrations/2017_02_18_174221_create_companies_table.php
+++ database/migrations/2017_02_18_174221_create_companies_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateCompaniesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('companies', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id')->unsigned();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('cpf')->unique();
-            $table->date('birthday');
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('cnpj')->unique();
+            $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('companies');
     }
 }
