6008bf944ca35178dd047afa03a7bd469a2e16bc
Juaum Cruz <juaum.cruz@gmail.com>
2017-02-18 18:00:06 -0300

    .

---
+++ app/Booking.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Booking extends Model
+{
+    protected $fillable = [
+        'user_id', 'voucher_id', 'value', 'used'
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function voucher() {
+        return $this->belongsTo(Voucher::class);
+    }
+}
---
+++ database/migrations/2017_02_18_181023_create_vouchers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVouchersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vouchers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('sale_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->string('code')->unique();
+            $table->boolean('active');
+            $table->date('booking_date');
+            $table->timestamps();
+            $table->foreign('sale_id')->references('id')->on('sales');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vouchers');
+    }
+}
